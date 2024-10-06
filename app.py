e59af8108725434bb9f594c52ecb6bf7883f6965
Kapeesh Kaul <kkapeesh1999@gmail.com>
2024-10-06 08:23:35 -0600

    changed port

--- app.py
+++ app.py
@@ -237,5 +237,5 @@ def get_planet_details():
     return jsonify(result)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5001))
     app.run(host='0.0.0.0', port=port, debug=False, threaded=True)
