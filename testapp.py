2eedb4fe53c7cc2a774b623a977f583898fc11df
Kapeesh Kaul <kkapeesh1999@gmail.com>
2024-10-06 09:39:43 -0600

    renamed categories

--- testapp.py
+++ testapp.py
@@ -1,12 +1,12 @@
 import requests
-
+from numpy import inf
 url = 'http://127.0.0.1:5001/get_top_planets'
 data = {
     # "filepath": 'PSCompPars.csv',
     # "SNR0": 100,
     # "D": 6,
-    # "top_n": 2,
-    # "SNR_filter": 100,
+    "top_n": 2,
+    "SNR_filter": -10000000000000,
     # "category": "hello"
 }
 
